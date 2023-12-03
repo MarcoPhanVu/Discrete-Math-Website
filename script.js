696d897f29384eb3ebc715a8c086b1a7931fb692
Marco <phanvutuananh75@gmail.com>
2023-12-04 00:00:19 +0700

    fill all datas and styling for them

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Say oh yeahhh")
+// console.log("Say oh yeahhh")
+
+backToTopBtn = document.querySelectorAll('.back-to-top');
+
+backToTopBtn.forEach(element => {
+    element.addEventListener("click", function() {
+        // console.log("clicked");
+        window.scrollTo(0, 0);
+    });
+});
